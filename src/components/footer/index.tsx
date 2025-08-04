af0bc1e97a8d01e548e57393934987b229888d26
yuraup <o721@naver.com>
2025-08-04 15:28:27 +0900

    <Style> 푸터 색상 변경

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,6 @@
 const Footer: React.FC = () => {
   return (
-    <div className="flex w-full justify-end bg-amber-300 p-4">
+    <div className="flex w-full justify-end bg-white p-4">
       factseeker1@gmail.com
     </div>
   );
