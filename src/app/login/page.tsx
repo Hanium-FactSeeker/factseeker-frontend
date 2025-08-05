efc86045e1ec6287a8b350d866f6c66f995e337f
leesj0188 <lsj01881@gmail.com>
2025-08-05 20:11:53 +0900

    Feat: [#1] 로그인 페이지의 별 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,15 +3,19 @@ import TextInput from "../../components/ui/button/TextInput"
 import { NaverLoginButton } from "../../components/ui/button/NaverLoginButton";
 import { KakaoLoginButton } from "../../components/ui/button/KakaoLoginButton";
 import { FaUser, FaLock } from "react-icons/fa";
+import Logo from "../../assets/Logo";
 
 export default function LoginPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-white">
       <div className="flex flex-col items-center gap-6">
         <div className="flex flex-col items-center gap-2">
+        <div>
+            <Logo width={30} height={30} className="mb-0" />
+        </div>  
           <span className="text-[20px] font-bold text-[#7B61FF]">Fact Seeker</span>
         </div>
-
+        
         <h2 className="text-xl font-bold text-black mt-4">로그인</h2>
 
         <div className="flex flex-col gap-5 mt-2">
@@ -24,8 +28,12 @@ export default function LoginPage() {
         </button>
 
         
-        <div className="text-xs text-black mt-1">
-          회원가입 | 아이디 찾기 | 비밀번호 찾기
+        <div className="flex items-center justify-center text-xs text-[#626262] font-[Pretendard] gap-2">
+            <span className="cursor-pointer">회원가입</span>
+            <span className="text-[#C4C4C4]">|</span>
+            <span className="cursor-pointer">아이디 찾기</span>
+            <span className="text-[#C4C4C4]">|</span>
+            <span className="cursor-pointer">비밀번호 찾기</span>
         </div>
 
         <div className="w-96 h-[1px] bg-[#E0E0E0] mt-[-4px]" />
