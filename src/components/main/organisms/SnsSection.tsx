14cd8283df80fe12dfa14e9ec7c7b29f78ad202e
yuraup <o721@naver.com>
2025-08-10 17:07:31 +0900

    <Feat> [#6] 메인페이지 sns 영역 캐러셀 추가

---
+++ src/components/main/organisms/SnsSection.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import SnsCard from '@/components/ui/sns/SnsCard';
+import SectionTitle from '@/components/ui/title/SectionTitle';
+import { snsData } from '@/constants/snsData';
+import { SnsType } from '@/types/logo';
+
+const SnsSection = () => {
+  return (
+    <div className="mx-auto h-auto w-[90%] overflow-hidden md:w-[1000px]">
+      <SectionTitle link="/sns" title="오늘의 정치 SNS 모아보기 &gt;" />
+      <Swiper
+        spaceBetween={12}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        modules={[Navigation, Pagination]}
+        breakpoints={{
+          0: { slidesPerView: 2, slidesPerGroup: 2 },
+          640: { slidesPerView: 3, slidesPerGroup: 3 },
+          1024: { slidesPerView: 4, slidesPerGroup: 4 },
+        }}
+      >
+        {snsData.map((item, idx) => (
+          <SwiperSlide key={idx} className={`${idx === 0 ? 'ml-[1%]' : ''}`}>
+            <SnsCard
+              name={item.name}
+              party={item.party}
+              type={item.type as SnsType}
+              percentage={item.percentage}
+              figureImg={item.figureImg ?? ''}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default SnsSection;
