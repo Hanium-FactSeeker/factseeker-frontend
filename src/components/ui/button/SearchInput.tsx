5fafa54e408582195f5d875dd7f3c5480139a02f
leesj0188 <lsj01881@gmail.com>
2025-08-12 00:30:58 +0900

    Fix: [#15] 검색창 Input 스타일링 변경

--- src/components/ui/button/SearchInput.tsx
+++ src/components/ui/button/SearchInput.tsx
@@ -15,7 +15,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     {
       iconLeft,
       iconRight,
-      inputSize = "lg", 
+      inputSize = "lg",
       className,
       fullWidth,
       disabled,
@@ -23,32 +23,37 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     },
     ref
   ) => {
+    const widthClass = fullWidth ? "w-full" : "w-full md:w-[708px]";
+
     return (
       <div
         className={clsx(
-          "relative",
-          fullWidth ? "w-full" : "w-[708px]",
+          "relative flex items-center",
+          widthClass,
           sizeMap[inputSize],
-          "flex items-center border border-[#D9DBDC] bg-white text-[#626262] font-pretendard",
+          "border border-gray-normal bg-white text-black-normal font-pretendard",
           disabled && "opacity-50 cursor-not-allowed",
           className
         )}
       >
         {iconLeft && (
-          <div className="pl-3 flex-shrink-0 text-[#D9DBDC]">{iconLeft}</div>
+          <div className="pl-3 flex-shrink-0 text-gray-normal">{iconLeft}</div>
         )}
+
         <input
           ref={ref}
           {...props}
           className={clsx(
-            "flex-1 bg-transparent outline-none placeholder:text-[#B0B0B0]",
+            "flex-1 bg-transparent outline-none",
+            "placeholder:text-gray-strong",
             iconLeft ? "pl-2" : "pl-4",
             iconRight ? "pr-10" : "pr-4"
           )}
           disabled={disabled}
         />
+
         {iconRight && (
-          <div className="absolute right-3 top-1/2 -translate-y-1/2 text-[#D9DBDC]">
+          <div className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-normal">
             {iconRight}
           </div>
         )}
